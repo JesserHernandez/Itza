aa5cb582f0488d876d63d31598b1bd64d59530ce
JesserHernandez <ph4931945@gmail.com>
2025-08-23 02:07:18 -0600

    feat(fortify): Adds the new attributes of the users migration

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -14,40 +14,64 @@ class CreateNewUser implements CreatesNewUsers
 {
     use PasswordValidationRules;
 
-    /**
-     * Create a newly registered user.
-     *
-     * @param  array<string, string>  $input
-     */
     public function create(array $input): User
     {
         Validator::make($input, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'name' => ['required', 'string', 'min:3', 'max:50'],
+            'surname' => ['required', 'string', 'min:3', 'max:50'],
+            'email' => ['required', 'string', 'email', 'min:3', 'max:50', 'unique:users'],
+            'gender' => ['required', 'string', 'min:3', 'max:10'],
+            'phone_number' => ['required', 'string', 'min:3', 'max:20'],
+            'identification_card' => ['required', 'string', 'min:3', 'max:20', 'unique:users'],
             'password' => $this->passwordRules(),
-            'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
+            'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : ''
         ])->validate();
 
         return DB::transaction(function () use ($input) {
             return tap(User::create([
                 'name' => $input['name'],
+                'surname' => $input['surname'],
                 'email' => $input['email'],
                 'password' => Hash::make($input['password']),
-            ]), function (User $user) {
-                $this->createTeam($user);
+                'gender' => $input['gender'],
+                'phone_number' => $input['phone_number'],
+                'identification_card' => $input['identification_card'],
+            ]), function ( $user) use ($input) {
+                $this->createTeam( $user,  $input);
             });
         });
     }
-
-    /**
-     * Create a personal team for the user.
-     */
-    protected function createTeam(User $user): void
+    protected function createTeam( User $user, array $input): void
     {
-        $user->ownedTeams()->save(Team::forceCreate([
-            'user_id' => $user->id,
-            'name' => explode(' ', $user->name, 2)[0]."'s Team",
+        if ($user->is_vendor) {
+            Validator::make($input, [
+                'name_team' => ['required', 'string', 'min:3', 'max:255'],
+                'address' => ['required', 'string', 'min:3', 'max:255'],
+                'type' => ['required', 'string', 'min:3', 'max:20'],
+                'city' => ['required', 'string', 'min:3', 'max:20'],
+                'municipality' => ['required', 'string', 'min:3', 'max:20'],
+                'phoneNumber' => ['required', 'string', 'max:20'],
+                'photo_path' => ['mimes:jpg,jpeg,png', 'max:1024']
+            ])->validate();
+
+            $user->ownedTeams()->save(Team::forceCreate([
+                'name_team' => $input['name_team'] ?? null,
+                'address' => $input['address'] ?? null,
+                'type' => $input['type'] ?? null,
+                'city' => $input['city'] ?? null,
+                'municipality' => $input['municipality'] ?? null,
+                'phoneNumber' => $input['phoneNumber'] ?? null,
+                'is_active' => true,
+                'personal_team' => true,
+                'user_id' => $user->id,
+            ]));
+        } else {
+            $user->ownedTeams()->save(Team::forceCreate([
+            'name_team' => explode(' ', $user->name, 2)[0]."'s Team",
+            'is_active' => false,
             'personal_team' => true,
-        ]));
+            'user_id' => $user->id,
+            ]));
+        }
     }
 }
