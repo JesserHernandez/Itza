5b5ce0ce816f09b630a907ef28279d082684847f
MaykelinG <maykelingg308@gmail.com>
2025-09-17 21:39:12 -0600

    fix(components): delete class of tailwind

--- resources/js/Components/InputLabel.vue
+++ resources/js/Components/InputLabel.vue
@@ -6,11 +6,11 @@ defineProps({
 </script>
 
 <template>
-    <label class="block font-medium text-sm text-gray-700">
+    <label class="">
         <p>
             <span v-if="value">{{ value }}</span>
             <span v-else><slot /></span>
-            <span>{{ texAdd }}</span>
+            <span class="asterisco">{{ texAdd }}</span>
         </p>
     </label>
 </template>
