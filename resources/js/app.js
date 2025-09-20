a697d6936191d737d0072acb7326a0e3c6ecd161
David Gonzalez <david.gonzalez5@tecnacional.edu.ni>
2025-09-19 18:12:14 -0600

    fix(app.js): new configuration for the system

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,7 +8,7 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 
-const appName = import.meta.env.VITE_APP_NAME || `Itz'at`;
+const appName = import.meta.env.VITE_APP_NAME || 'Itz´at';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
@@ -20,6 +20,6 @@ createInertiaApp({
             .mount(el);
     },
     progress: {
-        color: '#4B5563',
+        color: '#702b21',
     },
 });
