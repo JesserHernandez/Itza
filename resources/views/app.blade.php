969d31fab4c23907800b195570d71bad2251bb13
David Gonzalez <david.gonzalez5@tecnacional.edu.ni>
2025-09-29 17:45:03 -0600

    feat(app.blade.php): add favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="shortcut icon" href="/img/img-logo/recurso.png" type="image/x-icon" style="height: 40px; width: 40px;" class="favicon">
 
         <title inertia>{{ config('app.name', 'Itz´at') }}</title>
 
