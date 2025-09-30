b60519277bda43a9fdafb937f0538396499dc969
David Gonzalez <david.gonzalez5@tecnacional.edu.ni>
2025-09-30 14:15:06 -0600

    fix(applayouts): components removed

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -1,11 +1,9 @@
 <script setup>
 import { ref } from 'vue';
-import { Head, Link, router } from '@inertiajs/vue3';
-import ApplicationMark from '@/Components/ApplicationMark.vue';
-import Banner from '@/Components/Banner.vue';
-import Dropdown from '@/Components/Dropdown.vue';
-import DropdownLink from '@/Components/DropdownLink.vue';
 import NavLink from '@/Components/NavLink.vue';
+import Dropdown from '@/Components/Dropdown.vue';
+import { Head, Link, router } from '@inertiajs/vue3';
+import DropdownLink from '@/Components/DropdownLink.vue';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
 
 defineProps({
@@ -30,8 +28,6 @@ const logout = () => {
 <template>
     <div>
         <Head :title="title" />
-
-        <Banner />
 
         <div class="min-h-screen bg-gray-100">
             <nav class="bg-white border-b border-gray-100">
