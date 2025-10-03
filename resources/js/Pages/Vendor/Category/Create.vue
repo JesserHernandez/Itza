cc8ca83c4c75a3fdee5311bb1c68f5a2c38ece6f
David Gonzalez <david.gonzalez5@tecnacional.edu.ni>
2025-10-02 23:11:24 -0600

    feat(vendor/category/create): add view to create category

---
+++ resources/js/Pages/Vendor/Category/Create.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { Head } from "@inertiajs/inertia-vue3";
+import AppLayout from "@/Layouts/AppLayout.vue";
+import { useForm } from "@inertiajs/vue3";
+import Form from "@/Pages/Vendor/Category/Form.vue";
+
+
+
+const props = defineProps({
+    categories: Object,
+});
+
+// Método para enviar el formulario y mostrar el mensaje de éxito
+
+</script>
+
+<template>
+    <Head title="Crear categoría" />
+    <AppLayout title="Crear categoría" :href="route('categories.index')">
+        <div class="title-create">
+            <h1>Crear categoría</h1>
+        </div>
+
+        <Form :categories="categories" @success="showSuccessAlert" />
+    </AppLayout>
+</template>
