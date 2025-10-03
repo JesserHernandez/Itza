84b5b5e22a516e43999910918a7c1b2c26f0a6a8
David Gonzalez <david.gonzalez5@tecnacional.edu.ni>
2025-10-02 23:12:50 -0600

    feat(vendor/category/edit): add view to edit category

---
+++ resources/js/Pages/Vendor/Category/Edit.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { Head } from "@inertiajs/inertia-vue3";
+import AppLayout from "@/Layouts/AppLayout.vue";
+import Form from "@/Pages/Vendor/Category/Form.vue";
+
+const props = defineProps({
+    category: Object,
+});
+
+</script>
+
+<template>
+    <Head title="Editar categoría" />
+    <AppLayout title="Editar categoría" :href="route('categories.index')">
+        <div class="title-create">
+            <h1>Editar categoría</h1>
+        </div>
+
+        <Form :category="props.category" @success="showSuccessAlert" />
+    </AppLayout>
+</template>
