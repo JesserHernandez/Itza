38e99314b19936cbe72f1cb2921f58cfcae252b8
David Gonzalez <david.gonzalez5@tecnacional.edu.ni>
2025-10-02 23:13:22 -0600

    feat(vendor/productmaterial/edit): add view to edit productmaterial

---
+++ resources/js/Pages/Vendor/ProductMaterial/Edit.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { Head } from "@inertiajs/inertia-vue3";
+import AppLayout from "@/Layouts/AppLayout.vue";
+import Form from "@/Pages/Vendor/ProductMaterial";
+
+const props = defineProps({
+    productMaterial: Object,
+});
+</script>
+
+<template>
+    <Head title="Editar material"/>
+
+    <AppLayout :href="route('product_materials.index')">
+        <div class="title-create">
+            <h1>Editar material</h1>
+        </div>
+
+        <Form :productMaterial="props.productMaterial" @sucess="showSucessAlert"/>
+    </AppLayout>
+</template>
