cc8769fb53c0af756cc1e9fa2b0517bd73e847ab
David Gonzalez <david.gonzalez5@tecnacional.edu.ni>
2025-10-02 23:15:17 -0600

    feat(vendor/tag/show): add show view to display database records

---
+++ resources/js/Pages/Vendor/Tag/Show.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { Head } from "@inertiajs/vue3";
+import AppLayout from "@/Layouts/AppLayout.vue";
+
+const props = defineProps({
+    tag: Object, // Cambié productMaterial a tag
+});
+</script>
+
+<template>
+    <Head :title="tag.name" />
+    <AppLayout :href="route('tags.index')">
+        <div class="title-create">
+            <h1>Detalles de la etiqueta</h1>
+        </div>
+
+        <div class="container-form">
+            <div class="form">
+                <div class="items-form">
+                    <p><strong>Nombre: </strong>{{ tag.name }}</p>
+                </div>
+                <div class="items-form">
+                    <p>
+                        <strong>Descripción: </strong>
+                        {{ tag.description }}
+                    </p>
+                </div>
+            </div>
+        </div>
+    </AppLayout>
+</template>
