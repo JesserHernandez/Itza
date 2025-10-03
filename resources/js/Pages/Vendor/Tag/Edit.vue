bab21d91f079a639cbc714a91846c728dffe664d
David Gonzalez <david.gonzalez5@tecnacional.edu.ni>
2025-10-02 23:13:40 -0600

    feat(vendor/tag/edit): add view to edit tag

---
+++ resources/js/Pages/Vendor/Tag/Edit.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { Head } from "@inertiajs/inertia-vue3";
+import AppLayout from "@/Layouts/AppLayout.vue";
+import Form from "@/Pages/Vendor/Tag/Form.vue"; // Cambié el formulario al de tags
+
+const props = defineProps({
+    tag: Object, // Cambié productMaterial a tag
+});
+</script>
+
+<template>
+    <Head title="Editar etiqueta" />
+
+    <AppLayout :href="route('tags.index')">
+        <div class="title-create">
+            <h1>Editar etiqueta</h1>
+        </div>
+
+        <!-- Pasamos la etiqueta al formulario -->
+        <Form :tag="props.tag" @success="showSuccessAlert" />
+    </AppLayout>
+</template>
