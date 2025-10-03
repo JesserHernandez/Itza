01d09daa4164f1df104edfcaf72c16305186e2f7
David Gonzalez <david.gonzalez5@tecnacional.edu.ni>
2025-10-02 23:12:15 -0600

    feat(vendor/tag/create): add view to create tag

---
+++ resources/js/Pages/Vendor/Tag/Create.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { Head } from "@inertiajs/inertia-vue3";
+import AppLayout from "@/Layouts/AppLayout.vue";
+import Form from "@/Pages/Vendor/Tag/Form.vue";
+
+const props = defineProps({
+});
+</script>
+
+<template>
+    <Head title="Crear etiqueta" />
+    <AppLayout title="Crear etiqueta" :href="route('tags.index')">
+        <div class="title-create">
+            <h1>Crear etiqueta</h1>
+        </div>
+        
+        <Form :tag="tag" @success="showSuccessAlert"/>
+    </AppLayout>
+</template>
