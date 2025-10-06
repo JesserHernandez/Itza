d29c08feeb507a7750c0f20175f7d1cd5bada9ca
David Gonzalez <david.gonzalez5@tecnacional.edu.ni>
2025-10-06 14:12:40 -0600

    fix(applayouts): add dropdown list for the administration

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -12,6 +12,7 @@ defineProps({
 });
 
 const showingNavigationDropdown = ref(false);
+const isSubmenuOpen = ref(false);
 
 const switchToTeam = (team) => {
     router.put(
@@ -28,6 +29,10 @@ const switchToTeam = (team) => {
 const logout = () => {
     router.post(route("logout"));
 };
+
+function toggleSubmenu() {
+    isSubmenuOpen.value = !isSubmenuOpen.value;
+}
 </script>
 
 <template>
@@ -48,44 +53,152 @@ const logout = () => {
             <div class="items-aside">
                 <div class="actions-aside">
                     <NavLink :href="href" class="btn-class back">
-                        <img src="/icons/icons-interface/double-arrow-icon.svg" alt="" class="icons">
+                        <img
+                            src="/icons/icons-interface/double-arrow-icon.svg"
+                            alt=""
+                            class="icons"
+                        />
                         <span class="name-link"> Volver </span>
                     </NavLink>
 
                     <button class="open-close btn-class">
-                        <img src="/icons/icons-interface/button-menu-icon.svg" alt="" class="icons">
+                        <img
+                            src="/icons/icons-interface/button-menu-icon.svg"
+                            alt=""
+                            class="icons"
+                        />
                     </button>
                 </div>
                 <NavLink class="btn-class" :href="route('admin')">
-                    <img src="/icons/icons-interface/dashboard-icon.svg" alt="" class="icons">
+                    <img
+                        src="/icons/icons-interface/dashboard-icon.svg"
+                        alt=""
+                        class="icons"
+                    />
                     <span class="name-link"> Dashboard </span>
                 </NavLink>
                 <NavLink class="btn-class" :href="route('teams.create')">
-                    <img src="/icons/icons-interface/store-white-icon.svg" alt="" class="icons">
+                    <img
+                        src="/icons/icons-interface/store-white-icon.svg"
+                        alt=""
+                        class="icons"
+                    />
                     <span class="name-link"> Tiendas </span>
                 </NavLink>
-                <NavLink class="btn-class" :href="route('categories.index')">
-                    <img src="/icons/icons-interface/task-icon.svg" alt="" class="icons">
-                    <span class="name-link"> Categorías </span>
-                </NavLink>
-                <NavLink class="btn-class" :href="route('product_materials.index')">
-                    <img src="/icons/icons-ceramics/ceramic-mingle-icon.svg" alt="" class="icons">
-                    <span class="name-link"> Materiales </span>
-                </NavLink>
-                <NavLink class="btn-class" :href="route('tags.index')">
-                    <img src="/icons/icons-interface/tag-icon.svg" alt="" class="icons">
-                    <span class="name-link"> Tags </span>
-                </NavLink>
-                <NavLink class="btn-class">
-                    <img src="/icons/icons-ceramics/ceramic-7-icon.svg" alt="" class="icons">
+
+                <ul class=" title-list">
+                    <li class="item-list "
+                    >
+                        <button
+                            :class="{
+                                'btn-class': true,
+                                'active': isSubmenuOpen,
+                            }"
+                            @click="toggleSubmenu"
+                            aria-expanded="isSubmenuOpen"
+
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="8"
+                                height="4"
+                                viewBox="0 0 10 7"
+                                fill="none"
+                                class="icons"
+                            >
+                                <path
+                                    d="M5 6.83335L0 1.83335L1.16667 0.666687L5 4.50002L8.83333 0.666687L10 1.83335L5 6.83335Z"
+                                    fill="#F0F0F0"
+                                />
+                            </svg>
+                            <span>Administración</span>
+                        </button>
+
+                        <ul
+                            class="list-admin"
+                            v-show="isSubmenuOpen"
+                            :class="{ 'expanded': isSubmenuOpen, 'collapsed': !isSubmenuOpen }"
+                            :aria-hidden="!isSubmenuOpen"
+                        >
+                            <li>
+                                <NavLink
+                                    class="btn-class"
+                                    :href="route('categories.index')"
+                                >
+                                    <img
+                                        src="/icons/icons-interface/task-icon.svg"
+                                        alt=""
+                                        class="icons"
+                                    />
+                                    <span class="name-link"> Categorías </span>
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink
+                                    class="btn-class"
+                                    :href="route('product_materials.index')"
+                                >
+                                    <img
+                                        src="/icons/icons-ceramics/ceramic-mingle-icon.svg"
+                                        alt=""
+                                        class="icons"
+                                    />
+                                    <span class="name-link"> Materiales </span>
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink
+                                    class="btn-class"
+                                    :href="route('tags.index')"
+                                >
+                                    <img
+                                        src="/icons/icons-interface/tag-icon.svg"
+                                        alt=""
+                                        class="icons"
+                                    />
+                                    <span class="name-link"> Tags </span>
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink class="btn-class">
+                    <img
+                        src="/icons/icons-ceramics/ceramic-7-icon.svg"
+                        alt=""
+                        class="icons"
+                    />
                     <span class="name-link"> Productos </span>
                 </NavLink>
+
+                            </li>
+                            <li>
+                                <NavLink class="btn-class">
+                                    <img
+                                        src="/icons/icons-interface/post-white-icon.svg"
+                                        alt=""
+                                        class="icons"
+                                    />
+                                    <span class="name-link"> Publicaciones </span>
+                                </NavLink>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+
+
                 <NavLink class="btn-class">
-                    <img src="/icons/icons-interface/user-artist-white-icon.svg" alt="" class="icons">
+                    <img
+                        src="/icons/icons-interface/user-artist-white-icon.svg"
+                        alt=""
+                        class="icons"
+                    />
                     <span class="name-link"> Clientes </span>
                 </NavLink>
                 <NavLink class="btn-class">
-                    <img src="/icons/icons-interface/calendar-icon.svg" alt="" class="icons">
+                    <img
+                        src="/icons/icons-interface/calendar-icon.svg"
+                        alt=""
+                        class="icons"
+                    />
                     <span class="name-link"> Calendario </span>
                 </NavLink>
             </div>
