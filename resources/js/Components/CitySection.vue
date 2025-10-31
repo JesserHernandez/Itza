f3cd53bca25a6476ad8b4c4e1be2e90fe63d4f8a
MaykelinG <maykelingg308@gmail.com>
2025-10-31 11:51:26 -0600

    fix(component): new component for the city section

---
+++ resources/js/Components/CitySection.vue
@@ -0,0 +1,37 @@
+<script setup>
+defineProps({
+    image: String,
+    name: String,
+    description: String,
+})
+</script>
+
+<template>
+    <div class="city-section">
+
+        <div class="cards-artist">
+            <img :src="image" alt="" class="image">
+
+            <div class="info">
+
+                <div class="nombre">
+                    <img src="/icons/icons-interface/city-icon.png" alt=""><h2>{{ name }}</h2> 
+                </div>
+
+                <div class="description">
+                    <p>{{ description }}</p>
+                </div>
+
+                <div class="container-end">
+                   
+                    <div class="follow">
+                        <button class="btn-class">
+                            <span>Conocer más</span>
+                            <img src="/icons/icons-interface/conocer-icon.png" alt="" class="icons add">
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
