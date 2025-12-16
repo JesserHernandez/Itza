68fdd7e596cc64c5123716bb9ababf9757e1bd17
David Antonio González <dgonzaleza007@gmail.com>
2025-12-15 22:13:20 -0600

    feat(/status_fase): add the view for components under construction, their styles, and paths.

---
+++ resources/js/Pages/StatusFase.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { Head } from "@inertiajs/vue3";
+import { Link } from "@inertiajs/vue3";
+</script>
+
+<template>
+
+    <Head title="Fase 2 próximamente" />
+    <!-- <NavbarComponent /> -->
+    <div class="status-container">
+        <section class="status-picture">
+        </section>
+        <section class="status-content">
+            <h1>Esta sección se encuentra en construcción y estará lista en la fase 2 de ITZ´AT</h1>
+            <p>¡Gracias por tu comprensión!</p>
+        </section>
+        <section class="status-button">
+            <Link href="/" class="btn-class">
+                Regresar al inicio
+                <img src="/icons/icons-interface/next-white-icon.svg" alt="Botón para ir al inicio" />
+            </Link>
+        </section>
+    </div>
+</template>
